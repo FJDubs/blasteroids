df402a7b900d0e0bd77571197db708489c6915c6
FJDubs <taggartf1@gmail.com>
2024-09-04 17:35:57 -0400

    added player and draws play into game screen

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill((0,0,0))
     game_clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
+         # limit the framerate to 60 FPS
         dt = (game_clock.tick(60)) / 1000
 
 if __name__ == "__main__":
