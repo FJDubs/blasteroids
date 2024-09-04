92c4ab12fea2214fd2f3149f5336b0cf6287fdbe
FJDubs <taggartf1@gmail.com>
2024-09-04 16:45:53 -0400

    added game clock

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.fill((0,0,0))
+    game_clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
+        dt = (game_clock.tick(60)) / 1000
 
 if __name__ == "__main__":
     main()
