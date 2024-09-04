123f823ea8c729adcc1290d30c4073265cf9e6c7
FJDubs <taggartf1@gmail.com>
2024-09-04 18:37:03 -0400

    moved update and draw funcions to updatable and drawable groups

--- main.py
+++ main.py
@@ -7,14 +7,22 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
         screen.fill('black')
-        player.draw(screen)
+        for u in updatable:
+            u.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
          # limit the framerate to 60 FPS
         dt = (game_clock.tick(60)) / 1000
