76b5b04c0a8048e1bcc8c87e42b9925679c1d602
FJDubs <taggartf1@gmail.com>
2024-09-05 18:41:51 -0400

    shots destroy smallest asteroids and break apart larger asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,4 +12,16 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
-    
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        angle = random.uniform(20, 50)
+        child_velocity_one = self.velocity.rotate(angle)
+        child_velocity_two = self.velocity.rotate(-angle)
+        child_radius = self.radius - ASTEROID_MIN_RADIUS
+        child_one = Asteroid(self.position.x, self.position.y, child_radius)
+        child_two = Asteroid(self.position.x, self.position.y, child_radius)
+        child_one.velocity = child_velocity_one * 1.2
+        child_two.velocity = child_velocity_two * 1.2
